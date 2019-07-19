2a4856deca697f49ec1ac7eddaf40ff18c28e7e9
xivier <542763565@qq.com>
2019-07-19 17:45:38 +0800

    模块化

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,16 @@
 // import './class/lesson1'
-import 'babel-polyfill' //兼容库，处理兼容性，用于 es7的提案
-import './class/lesson16'
+// import 'babel-polyfill' //兼容库，处理兼容性，用于 es7的提案
+// import './class/lesson17'
+
+// import {A} from './class/lesson17'
+// import * as lesson from './class/lesson17'
+//
+// // console.log(A);
+// console.log(lesson.A);
+
+import lesson17 from './class/lesson17'
+
+console.log(lesson17.A);
+lesson17.test();
+let obj = new lesson17.Hello();
+obj.test();
