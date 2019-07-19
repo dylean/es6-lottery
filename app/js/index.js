28a59d2857fb48f1525001763ecd2e28b8facb15
xivier <542763565@qq.com>
2019-07-19 21:37:08 +0800

    html/css

--- app/js/index.js
+++ app/js/index.js
@@ -1,16 +1,2 @@
-// import './class/lesson1'
-// import 'babel-polyfill' //兼容库，处理兼容性，用于 es7的提案
-// import './class/lesson17'
-
-// import {A} from './class/lesson17'
-// import * as lesson from './class/lesson17'
-//
-// // console.log(A);
-// console.log(lesson.A);
-
-import lesson17 from './class/lesson17'
-
-console.log(lesson17.A);
-lesson17.test();
-let obj = new lesson17.Hello();
-obj.test();
+import 'babel-polyfill'; //兼容库，处理兼容性，用于 es7的提案
+import Lottery from './lottery';
